0791a0e6ad50135fbc198f3d20e56e321cf91e17
David Uribe <daviduribe998@gmail.com>
2022-10-31 00:32:42 -0300

    UPDATE: Redefine languages structure

--- core/domain/language/language.py
+++ core/domain/language/language.py
@@ -2,19 +2,33 @@ from enum import Enum
 from pydantic import BaseModel
 
 
-class LanguageTraits(BaseModel):
-    source: bool | None = False
-    target: bool | None = False
-    tts: bool | None = False
-
-
 class LanguageModel(str, Enum):
     ML = "ml"
     CLOUD = "cloud"
 
 
+class LanguageFeature(str, Enum):
+    TRANSLATE = "translate"
+    TTS = "tts"
+
+
+class LanguageAvailabilityMode(str, Enum):
+    INCLUDE = "include"
+    EXCLUDE = "exclude"
+
+
+class LanguageAvailability(BaseModel):
+    mode: LanguageAvailabilityMode | None = LanguageAvailabilityMode.EXCLUDE
+    sources: set[str] = set()
+    targets: set[str] = set()
+
+
+class LanguageModelSettings(BaseModel):
+    features: set[LanguageFeature] = set()
+    availability: LanguageAvailability | bool = True
+
+
 class Language(BaseModel):
     name: str
     code: str
-    models: list[LanguageModel] = [LanguageModel.CLOUD]
-    traits: LanguageTraits | None = LanguageTraits()
+    models: dict[LanguageModel, LanguageModelSettings | bool] = dict()
