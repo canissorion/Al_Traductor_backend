173fb101eeb1064d5520a4ebca4f2ec8225e0f59
Francisco garcía <francisco.garciag@utem.cl>
2022-11-23 23:22:10 -0300

    ADD
    configuracion de produccion

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         "main:app",
         host="0.0.0.0",
         port=8000,
-        debug=True,
-        reload=True,
+        debug=False,
+        reload=False,
         log_level="info",
     )
